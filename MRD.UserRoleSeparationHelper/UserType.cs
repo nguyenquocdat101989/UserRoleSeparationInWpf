939bac14c07f9e6edd957bf1a8b3731620fd42ef
Game Master <friendlyhospital@gmail.com>
2021-03-11 14:42:23 +0700

    Add some UserType for demo

---
+++ MRD.UserRoleSeparationHelper/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MRD.UserRoleSeparationHelper
+{
+    [Flags]
+    public enum UserType
+    {
+        Guest = UserAction.None,
+        User = UserAction.Read,
+        Manager = User | UserAction.Edit,
+        Admin = Manager | UserAction.Add | UserAction.Delete
+    }
+}
