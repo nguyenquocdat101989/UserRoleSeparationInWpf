963297447aaf8341ec10d37443bba60f4fba0f59
Game Master <friendlyhospital@gmail.com>
2021-03-11 14:41:37 +0700

    Add UserAction enum

---
+++ MRD.UserRoleSeparationHelper/UserAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MRD.UserRoleSeparationHelper
+{
+    [Flags]
+    public enum UserAction
+    {
+        None = 1,
+        Read = 2,
+        Edit = 4,
+        Add = 8,
+        Delete = 16
+    }
+}
