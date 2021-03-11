97f584b2f56750870441030eeaea98f9bf0ee1c3
Game Master <friendlyhospital@gmail.com>
2021-03-11 15:15:36 +0700

    Complete simple demo

--- MRD.Demo/MainWindow.xaml.cs
+++ MRD.Demo/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MRD.UserRoleSeparationHelper;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,43 @@ namespace MRD.Demo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private UserType userPermission;
+
+        public UserType LoggedInUserType
+        {
+            get { return userPermission; }
+            set
+            {
+                userPermission = value;
+                OnPropertyChanged(nameof(LoggedInUserType));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            LoggedInUserType = UserType.Guest;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoginAsAdmin(object sender, RoutedEventArgs e)
+        {
+            LoggedInUserType = UserType.Admin;
+        }
+
+        private void LoggOut(object sender, RoutedEventArgs e)
+        {
+            LoggedInUserType = UserType.Guest;
         }
     }
 }
